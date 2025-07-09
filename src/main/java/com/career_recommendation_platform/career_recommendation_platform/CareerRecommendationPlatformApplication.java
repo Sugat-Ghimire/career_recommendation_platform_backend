9bb56b30757f23b5816d21d313b4b75f3542d34e
Sugat Ghimire <sugat1237@gmail.com>
2025-07-09 14:28:45 +0545

    first commit

---
+++ src/main/java/com/career_recommendation_platform/career_recommendation_platform/CareerRecommendationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.career_recommendation_platform.career_recommendation_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerRecommendationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerRecommendationPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/career_recommendation_platform/career_recommendation_platform/CareerRecommendationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.career_recommendation_platform.career_recommendation_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerRecommendationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
